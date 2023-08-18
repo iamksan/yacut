ddfe25064643e1a6348ec18db9502b025a897e0e
Никита Остапенко <nikitaostapenko0968@gmail.com>
2023-08-18 22:15:36 +0300

    2rev

--- settings.py
+++ settings.py
@@ -6,7 +6,8 @@ CUSTOM_ID_PATTERN = r'^[A-Za-z0-9]+$'
 STATIC_DIR = os.path.abspath('./html')
 TEMPLATE_DIR = os.path.join(STATIC_DIR, 'templates')
 
+
 class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', 'MY_SECRET_KEY') 
+    SECRET_KEY = os.getenv('SECRET_KEY', 'MY_SECRET_KEY')
