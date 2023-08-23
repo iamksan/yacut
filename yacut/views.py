7bb4f910f9ba09b8caee2b6744a939e4e5c74a44
Никита Остапенко <nikitaostapenko0968@gmail.com>
2023-08-23 10:09:50 +0300

    3rev

--- yacut/views.py
+++ yacut/views.py
@@ -1,5 +1,6 @@
 from flask import flash, redirect, render_template
 from . import app, db
+
 from .forms import LinkForm
 from .models import URLMap
 from .utils import get_unique_short_id, correct_short
@@ -8,25 +9,26 @@ from .utils import get_unique_short_id, correct_short
 @app.route('/', methods=['GET', 'POST'])
 def index_view():
     form = LinkForm()
-    if form.validate_on_submit():
-        custom_id = form.custom_id.data
-        if custom_id:
-            if URLMap.query.filter_by(short=custom_id).first():
-                flash(f'Имя {custom_id} уже занято!')
-                return render_template('main.html', form=form)
-            elif not correct_short(custom_id):
-                flash('Указано недопустимое имя для короткой ссылки')
-                return render_template('main.html', form=form)
-        else:
-            custom_id = get_unique_short_id()
-        link = URLMap(
-            original=form.original_link.data,
-            short=custom_id
-        )
-        db.session.add(link)
-        db.session.commit()
-        return render_template('main.html', form=form, link=link)
-    return render_template('main.html', form=form)
+    custom_id = form.custom_id.data
+    if not form.validate_on_submit():
+        return render_template('main.html', form=form)
+    elif custom_id:
+        if URLMap.query.filter_by(short=custom_id).first():
+            flash(f'Имя {custom_id} уже занято!')
+            return render_template('main.html', form=form)
+        elif not correct_short(custom_id):
+            flash('Указано недопустимое имя для короткой ссылки')
+            return render_template('main.html', form=form)
+    else:
+        custom_id = get_unique_short_id()
+    link = URLMap(
+        original=form.original_link.data,
+        short=custom_id
+    )
+    db.session.add(link)
+    db.session.commit()
+    return render_template('main.html', form=form, link=link)
+
 
 
 @app.route('/<path:link>')
